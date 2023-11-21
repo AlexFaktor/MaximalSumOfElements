46619ae6ea5d23ecc0cc031d4fb341610a429d0e
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2023-11-21 14:32:42 +0200

    Initial commit local

---
+++ Maximal sum of elements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Maximal_sum_of_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
